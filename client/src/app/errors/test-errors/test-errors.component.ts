e1f724a29c688d5f5198719b547af6dd62b0d7db
Terran Adams <terranadams@Terrans-MacBook-Air.local>
2024-08-23 12:37:06 -0600

    lesson 75

--- client/src/app/errors/test-errors/test-errors.component.ts
+++ client/src/app/errors/test-errors/test-errors.component.ts
@@ -6,44 +6,48 @@ import { Component, inject } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './test-errors.component.html',
-  styleUrl: './test-errors.component.css'
+  styleUrl: './test-errors.component.css',
 })
 export class TestErrorsComponent {
   baseUrl = 'https://localhost:5001/api/';
-  private http = inject(HttpClient)
+  private http = inject(HttpClient);
+  validationErrors: string[] = [];
 
   get400Error() {
     this.http.get(this.baseUrl + 'buggy/bad-request').subscribe({
-      next: res => console.log(res),
-      error: error => console.log(error)
-    })
+      next: (res) => console.log(res),
+      error: (error) => console.log(error),
+    });
   }
 
   get401Error() {
     this.http.get(this.baseUrl + 'buggy/auth').subscribe({
-      next: res => console.log(res),
-      error: error => console.log(error)
-    })
+      next: (res) => console.log(res),
+      error: (error) => console.log(error),
+    });
   }
 
   get404Error() {
     this.http.get(this.baseUrl + 'buggy/not-found').subscribe({
-      next: res => console.log(res),
-      error: error => console.log(error)
-    })
+      next: (res) => console.log(res),
+      error: (error) => console.log(error),
+    });
   }
 
   get500Error() {
     this.http.get(this.baseUrl + 'buggy/server-error').subscribe({
-      next: res => console.log(res),
-      error: error => console.log(error)
-    })
+      next: (res) => console.log(res),
+      error: (error) => console.log(error),
+    });
   }
 
   get400ValidationError() {
     this.http.post(this.baseUrl + 'account/register', {}).subscribe({
-      next: res => console.log(res),
-      error: error => console.log(error)
-    })
+      next: (res) => console.log(res),
+      error: (error) => {
+        console.log(error);
+        this.validationErrors = error;
+      },
+    });
   }
 }
