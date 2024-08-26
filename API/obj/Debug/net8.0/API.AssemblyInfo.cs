9c3929947f0c42106b8c95f1e358ec6ebe35db16
Terran Adams <terranadams@Terrans-MacBook-Air.local>
2024-08-26 14:37:41 -0600

    lesson 83

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e56655e35724180778ceaf4f0e32c452824e5951")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35029a84a7abf49ca388d355f1e9fc605d332eae")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
