1850420b236d0aa0a2a6c55dffd52d784d88089c
Terran Adams <terranadams@Terrans-MacBook-Air.local>
2024-08-27 11:23:00 -0600

    actual lexxon 99

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b6e11f34cd262442ef10fdb10adf60f1f081378")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93545890e81c35a5778742d3acf55c444a75b570")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
