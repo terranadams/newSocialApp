2c4f5b498c4e921d9ebb981b3dbbbe4927266265
Terran Adams <terranadams@Terrans-MacBook-Air.local>
2024-08-27 16:30:04 -0600

    reset to lesson 105, screw the photos thing

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03908770f4a73e5501ed1b086b0dbf6fe79371d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c8a8a5d2c8521940c0f5da0bd0e14611388cb8e")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
