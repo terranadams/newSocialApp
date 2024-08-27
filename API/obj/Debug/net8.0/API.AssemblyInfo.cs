93545890e81c35a5778742d3acf55c444a75b570
Terran Adams <terranadams@Terrans-MacBook-Air.local>
2024-08-27 10:16:10 -0600

    lesson 99

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7883e7dcbcd3d35c9f2b32c0cb083f3e3923ebc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b6e11f34cd262442ef10fdb10adf60f1f081378")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,6 +1,5 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RegisterComponent } from '../register/register.component';
-import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -9,14 +8,9 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent implements OnInit {
-  http = inject(HttpClient); // new way of injecting without constructor
+export class HomeComponent {
   registerMode = false;
-  users: any;
 
-  ngOnInit(): void {
-    this.getUsers()
-  }
 
   registerToggle() {
     this.registerMode = !this.registerMode;
@@ -26,11 +20,4 @@ export class HomeComponent implements OnInit {
     this.registerMode = event;
   }
 
-  getUsers() {
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: (res) => (this.users = res),
-      error: (error) => console.log(error),
-      complete: () => console.log('Request has completed :)'),
-    });
-  }
 }
