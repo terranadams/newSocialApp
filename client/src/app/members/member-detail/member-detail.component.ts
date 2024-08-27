2c8a8a5d2c8521940c0f5da0bd0e14611388cb8e
Terran Adams <terranadams@Terrans-MacBook-Air.local>
2024-08-27 16:25:18 -0600

    Revert "lesson 105"
    This reverts commit c7f4f849c1296ebcb100d0fc58e18944a17e9df2.

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -2,13 +2,11 @@ import { Component, inject, OnInit } from '@angular/core';
 import { MembersService } from '../../_services/members.service';
 import { ActivatedRoute } from '@angular/router';
 import { Member } from '../../_models/member';
-import { TabsModule } from 'ngx-bootstrap/tabs';
-
 
 @Component({
   selector: 'app-member-detail',
   standalone: true,
-  imports: [TabsModule],
+  imports: [],
   templateUrl: './member-detail.component.html',
   styleUrl: './member-detail.component.css'
 })
@@ -23,7 +21,7 @@ export class MemberDetailComponent implements OnInit {
 
   loadMember() {
     const username = this.route.snapshot.paramMap.get('username');
-
+    
     if (!username) return;
     this.memberService.getMember(username).subscribe({
       next: member => this.member = member
