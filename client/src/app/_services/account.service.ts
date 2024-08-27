0b6e11f34cd262442ef10fdb10adf60f1f081378
Terran Adams <terranadams@Terrans-MacBook-Air.local>
2024-08-27 10:07:22 -0600

    lesson 97

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -1,14 +1,15 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable, signal } from '@angular/core';
-import { User } from '../_models/User';
+import { User } from '../_models/user';
 import { map } from 'rxjs';
+import { environment } from '../../environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
   private http = inject(HttpClient);
-  baseUrl = 'https://localhost:5001/api/';
+  baseUrl = environment.apiUrl;
   currentUser = signal<User | null>(null);
 
   login(model: any) {
