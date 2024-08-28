329e59ad9c84686fc710229ebc76139af1483b11
Terran Adams <terranadams@Terrans-MacBook-Air.local>
2024-08-28 10:35:44 -0600

    lesson 116

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 import { environment } from '../../environments/environment.development';
 import { Member } from '../_models/member';
+import { of, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -9,16 +10,25 @@ import { Member } from '../_models/member';
 export class MembersService {
   private http = inject(HttpClient);
   baseUrl = environment.apiUrl;
+  members = signal<Member[]>([]); // in other components when we try to refer this value, we'd do so by calling it, like memberService.members()
 
   getMembers() {
-    return this.http.get<Member[]>(this.baseUrl + 'users');
+    return this.http.get<Member[]>(this.baseUrl + 'users').subscribe({
+      next: members => this.members.set(members)
+    })
   }
 
   getMember(username: string) {
+    const member = this.members().find(x => x.userName === username);
+    if (member !== undefined) return of(member); // this returns the member as an observable
+
     return this.http.get<Member>(this.baseUrl + 'users/' + username);
   }
 
   updateMember(member: Member) {
-    return this.http.put(this.baseUrl + 'users', member)
+    return this.http.put(this.baseUrl + 'users', member).pipe(
+      tap(() => this.members.update(members => members.map(m => m.userName === member.userName ? member : m)))
+      // we're mapping through our members array, and replacing whichever one has a matching username, anything listening to the 'members' signal will get updated
+    )
   }
 }
